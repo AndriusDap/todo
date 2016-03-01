9d5b6f4c80e3c4d9daca316881fdcf57f1a5d5ff
AndriusDap <dapsevicius.andrius@gmail.com>
2016-03-01 22:06:42 +0200

    adding material-ui

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var injectTapEventPlugin = require('react-tap-event-plugin');
 var TodoItem = require('./TodoItem.jsx');
 
+
+
+injectTapEventPlugin();
 ReactDOM.render(
 	<TodoItem></TodoItem>,
 	document.getElementById('body')
